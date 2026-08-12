a2b08545b7e3d9df17b3bf9eed22c9b043ed4e59
Areeba Ali Siddiqui <areebaalisiddiqui@gmail.com>
2026-08-12 21:19:45 +0500

    feat: add persistent Chroma vector store

---
+++ vectorstore/chroma_store.py
@@ -0,0 +1,29 @@
+#take the vectors, chunks and metadata and store them in the chroma vectorstore 
+# so that we can retrieve them later for semantic search.
+"""
+1. Creating a persistent Chroma client
+2. Creating/getting a collection/table
+3. Create a function to add chunks, vectors, and metadata to the collection,The IDs should also be generated.
+"""
+import chromadb
+from datetime import datetime
+
+client = chromadb.PersistentClient(path="./chroma_db")
+
+collection = client.get_or_create_collection(
+    name="documents",
+    embedding_function=None,
+    metadata={
+            "description": "my first Chroma collection",
+            "created": str(datetime.now())
+        }
+)
+def add_chunks(chunks, vectors, metadata):
+    ids = [f"id_{i}" for i in range(len(chunks))]
+    collection.add(
+        ids=ids,
+        embeddings=vectors,
+        documents=chunks,
+        metadatas=metadata
+    )
+    return collection
